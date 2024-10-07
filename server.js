da0cb2c61f9bfc7be392f0ddcaf4274ae03caea9
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-10-07 14:06:59 +0530

    added sample express server for jenkins

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to Sample express server");
+});
+
+app.listen(3000, () => console.log("Server running at 3000"));
